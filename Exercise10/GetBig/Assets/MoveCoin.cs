ab70846c348763d28395900737d557ba5d65ed76
Jedian <jmb15@inf.ufpr.br>
2019-01-16 13:48:00 +0100

    Finished advanced 10
    Signed-off-by: Jedian <jmb15@inf.ufpr.br>

---
+++ Exercise10/GetBig/Assets/MoveCoin.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCoin : MonoBehaviour
+{
+    private GameObject Ball_ref;
+    private float Orig_y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ball_ref = GameObject.Find("Ball");
+        Orig_y = this.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, -Mathf.Sin(Time.time*2)/33));
+        transform.Rotate(Vector3.back*10);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Ball_ref)
+        {
+            Destroy(this.gameObject);
+            other.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+        }
+    }
+}
---
+++ Exercise10/GetBig/Assets/SpawnCoins.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCoins : MonoBehaviour
+{
+    public GameObject Coin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnCoin", 0, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnCoin()
+    {
+        Vector3 vector3 = new Vector3(Random.Range(-4, 4), Random.Range(0, 3), Random.Range(-4, 4));
+        Object.Instantiate(Coin, vector3, Coin.gameObject.transform.rotation);
+        if (this.transform.localScale.x > 0.5f) this.transform.localScale *= 0.99f;
+    }
+}
